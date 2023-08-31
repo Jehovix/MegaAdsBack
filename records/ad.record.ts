22116581fb2e2918215e3c30d8ae15b92e61ce4f
Jehovix <bialobrzeskipiotrkadzidlo@gmail.com>
2023-08-31 11:34:12 +0200

    Added tests and implementation of getting of one AdRecord.

--- records/ad.record.ts
+++ records/ad.record.ts
@@ -1,9 +1,9 @@
 import {AdEntity} from "../types/ad/ad-entity";
 import {ValidationError} from "../utils/errors";
+import {pool} from "../utils/db";
+import {FieldPacket} from "mysql2";
 
-interface NewAdEntity extends Omit<AdEntity, 'id'> {
-    id?: string;
-}
+type AdRecordResults = [AdEntity[], FieldPacket[]];
 
 export class AdRecord implements AdEntity {
     public id: string;
@@ -35,12 +35,21 @@ export class AdRecord implements AdEntity {
             throw new ValidationError('Nie można zlokalizować ogłoszenia.');
         }
 
-        this.name = obj.name
-        this.description = obj.description
-        this.price = obj.price
-        this.url = obj.url
-        this.lat = obj.lat
-        this.lon = obj.lon
+        this.id = obj.id;
+        this.name = obj.name;
+        this.description = obj.description;
+        this.price = obj.price;
+        this.url = obj.url;
+        this.lat = obj.lat;
+        this.lon = obj.lon;
+    }
+
+    static async getOne(id: string): Promise<AdRecord | null> {
+        const [results] = await pool.execute("SELECT * FROM `ads` WHERE id = :id", {
+            id,
+        }) as AdRecordResults;
+
+        return results.length === 0 ? null : new AdRecord(results[0]);
     }
 
 }
